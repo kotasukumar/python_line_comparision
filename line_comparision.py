c717b5f97f4162b2391525ac31d88c94972e69ab
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-08-21 21:55:36 +0530

    oops

--- line_comparision.py
+++ line_comparision.py
@@ -1,16 +1,23 @@
-def calculate(x1, x2, y1, y2):
-    return (x2-x1 ** 2 + y2-y1 ** 2) ** 0.5
+class Line:
+    def __init__(self, x1, x2, y1, y2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        self.calculate(x1, x2, y1, y2)
 
+    def calculate(x1, x2, y1, y2):
+        return (x2 - x1 ** 2 + y2 - y1 ** 2) ** 0.5
 
-def compare(length):
-    if length[0] > length[1]:
-        print("Line 1 is greater then line 2")
-    elif length[0] > length[1]:
-        print("Line 2 is greater then line 1")
-    elif length[0] == length[1]:
-        print("Both are line are equal in length")
-    else:
-        print("Some where gone wrong please try again")
+    def compare(length):
+        if length[0] > length[1]:
+            print("Line 1 is greater then line 2")
+        elif length[0] > length[1]:
+            print("Line 2 is greater then line 1")
+        elif length[0] == length[1]:
+            print("Both are line are equal in length")
+        else:
+            print("Some where gone wrong please try again")
 
 
 if __name__ == "__main__":
@@ -22,6 +29,6 @@ if __name__ == "__main__":
         x2 = int(input("Enter x2 point: "))
         y1 = int(input("Enter y1 point: "))
         y2 = int(input("Enter y2 point: "))
-        length.append(calculate(x1, x2, y1, y2))
+        length.append(Line.calculate(x1, x2, y1, y2))
     print(length)
-    compare(length)
+    Line.compare(length)
