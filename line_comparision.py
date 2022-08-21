bac320519d87a96046ab3f9967edf5577dfdda5f
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-08-21 21:39:15 +0530

    comparing length

--- line_comparision.py
+++ line_comparision.py
@@ -2,9 +2,21 @@ def calculate(x1, x2, y1, y2):
     return (x2-x1 ** 2 + y2-y1 ** 2) // 2
 
 
-print("Welcome to line comparison problem")
-x1 = int(input("Enter x1 point: "))
-x2 = int(input("Enter x2 point: "))
-y1 = int(input("Enter y1 point: "))
-y2 = int(input("Enter y2 point: "))
-print(calculate(x1, x2, y1, y2))
+def compare(length):
+    if length[0] == length[1]:
+        print("Both are line are equal in length")
+    else:
+        print("Both are line are not equal in length")
+
+
+if __name__ == "__main__":
+    length = []
+    print("Welcome to line comparison problem")
+    for number in range(1, 3):
+        print(f"Enter values of line {number}")
+        x1 = int(input("Enter x1 point: "))
+        x2 = int(input("Enter x2 point: "))
+        y1 = int(input("Enter y1 point: "))
+        y2 = int(input("Enter y2 point: "))
+        length.append(calculate(x1, x2, y1, y2))
+    compare(length)
